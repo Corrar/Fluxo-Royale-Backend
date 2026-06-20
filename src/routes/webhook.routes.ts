b4d2d8dee66cced9b31b987f7ff4eaef3dddf112
Corrar <brunocorral205@gmail.com>
2026-06-20 18:56:16 -0300

    Add GET route for activating Drive alarm
    Added a new GET route for activating the Drive alarm.

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
-// Importamos a função principal que criámos no controlador
-import { handleDriveWebhook } from '../controllers/webhook.controller';
+// Importamos as duas funções do controlador
+import { handleDriveWebhook, ativarAlarmeDoDrive } from '../controllers/webhook.controller';
 
 const router = Router();
 
 /**
- * ROTA: POST /drive
- * URL Final (exemplo): https://api.fluxo-royale.com.br/api/webhooks/drive
- * * É este o endereço exato que teremos de fornecer ao Google Cloud 
- * quando formos registar o Webhook (Passo 1).
+ * ROTA 1: POST /drive
+ * Esta é a porta que o Google vai "bater" quando houver uma alteração.
  */
 router.post('/drive', handleDriveWebhook);
+
+/**
+ * ROTA 2: GET /setup (O "Botão" de Ligar o Alarme)
+ * Esta é a rota que tu vais acessar pelo navegador uma única vez
+ * para avisar o Google que ele deve começar a vigiar a pasta.
+ */
+router.get('/setup', ativarAlarmeDoDrive);
 
 export default router;
